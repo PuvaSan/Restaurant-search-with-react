b6956a0719eaeb0773819ac9f5d3936d147a5937
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-09-06 17:59:32 +0900

    learned basic component rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,18 @@
 import './index.css';
+import * as ReactDOM from "react-dom/client";
+
+function Hello({name, age}) {
+
+  // var destructuring - can put inside the params
+  //const {name, age} = props;
+
+  return (
+    <div>
+      <h1>Hello, I am {name}, I am {age} years old</h1>
+    </div>
+  );
+}
+
+const rootContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(rootContainer);
+root.render(<Hello name="Max" age="21"/>);

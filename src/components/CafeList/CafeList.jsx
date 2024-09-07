1e539a4673c7ba6094de2eee51b6b0fb63c747f0
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-09-07 16:59:02 +0900

    fixed issue with array

--- src/components/CafeList/CafeList.jsx
+++ src/components/CafeList/CafeList.jsx
@@ -2,7 +2,8 @@ import "./CafeList.css";
 import Cafe from "../Cafe/Cafe";
 import React from "react";
 
-function CafeList(cafes) {
+function CafeList({cafes}) {
+  console.log(cafes);
   return (
     <div className="cafe-list">
       { cafes.map( cafe => <Cafe cafe={cafe} key={cafe.title} />)}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as ReactDOM from 'react-dom/client';
 
-const rootDiv = document.querySelector("#react-root");
-const root = ReactDOM.createRoot(rootDiv);
-root.render(<App />);
+const container = document.getElementById('root');
+
+// Make sure container is not null
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
+} else {
+  console.error("Root element not found");
+}
